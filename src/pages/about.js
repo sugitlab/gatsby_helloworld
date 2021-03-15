1db1ac496cbfc6eb9969f991d8b5aeecef6458d1
sugitlab <sgmt.snj@gmail.com>
2021-03-16 00:30:04 +0900

    tutorial_part_one_to_three

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+    </Layout>
+  )
+}
